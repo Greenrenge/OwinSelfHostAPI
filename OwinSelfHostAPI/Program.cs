74f26d30ed9948866b5102ad8d1aa1b67ae194a6
GreenMainPCAtHome <greenrenge@gmail.com>
2017-08-22 23:18:19 +0700

    Complete Self-Host WebApi

--- OwinSelfHostAPI/Program.cs
+++ OwinSelfHostAPI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace OwinSelfHostAPI
     {
         static void Main(string[] args)
         {
+            var startOption = new StartOptions("http://localhost:1234/");
+            startOption.Urls.Add("http://www.green.com:80/");
+            using (WebApp.Start<Startup>(startOption))
+            {
+                //https://stackoverflow.com/questions/4019466/httplistener-access-denied
+                //view all user in computer-> wmic useraccount list full
+                //To Reserve URL (run as Admin)
+                //netsh http add urlacl http://localhost:1234/ user=green
+                //netsh http add urlacl http://www.green.com:80/ user=green
+                Console.WriteLine("Server is currently running");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ OwinSelfHostAPI/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+//[assembly: OwinStartup(typeof(OwinSelfHostAPI.Startup))]
+namespace OwinSelfHostAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)  
+        {
+            HttpConfiguration httpConfig = new HttpConfiguration(); //สร้าง Config ของ Host 
+            httpConfig.Routes.MapHttpRoute( //สร้าง route 
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            //app.Use<>
+            app.UseWebApi(httpConfig);//extension method
+        }
+    }
+}
